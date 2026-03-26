87b2a94db8fe4536b0d3c1f6770515f543a4bfd0
Jeremi <jeremi_P@op.pl>
2026-03-26 18:03:33 +0100

    test

--- src/components/Projekty.jsx
+++ src/components/Projekty.jsx
@@ -1,40 +1,41 @@
 
 import { useEffect, useState } from "react";
-import logo2137 from "../assets/logo2137.png";
 
 const Projekty = () => {
+    const [projekty, setProjekty] = useState([])
+
+
     useEffect(() => {
         const pobierz = async () => {
             const projektyUrl = "http://localhost:3000/projects"
             const res = await fetch(projektyUrl)
-            const projekty = await res.json()
-            console.log(projekty)
+            const projektys = await res.json()
+            setProjekty(p => projektys)
         }
         console.log('test')
         pobierz()
     }, [])
 
-    const projekty = []
 
   return (
     <section className="sekcja-projekty">
       <h1>Oto moje Projekty</h1>
-      <ul>
+      <ul className="lista-projektow">
         {projekty.map((e) => (
-          <li key={`${e.name}-${e.date}`}>
-            <h4>{e.name}</h4>
-            <h6>{e.description}</h6>
-            <a href={e.url} target="_blank" rel="noreferrer">
+          <li key={`${e.name}-${e.date}`} className="karta-projektu">
+            <h4 className="tytul-projektu">{e.name}</h4>
+            <p className="opis-projektu">{e.description}</p>
+            <a className="link-projektu" href={e.url} target="_blank" rel="noreferrer">
               to jest link albo nie bo nie wszystkie dzialaja
             </a>
             <img 
-              style={{width:"60%"}}
+              className="obraz-projektu"
               src={e.img}
               alt={`Podglad projektu ${e.name}`}
             />
-            <h6>
+            <p className="meta-projektu">
               ten projekt zostal zrobiony w {e.date} i jest w statusie {e.state}
-            </h6>
+            </p>
           </li>
         ))}
       </ul>
