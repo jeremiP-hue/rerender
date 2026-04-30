ddd012ba58684d98cd9c590325b0aadfc055b81d
Jeremi <jeremi_P@op.pl>
2026-04-30 17:38:45 +0200

    po zajęciach 30.04.26

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,52 +1,108 @@
 import { useEffect, useState } from "react";
-const apiBaseUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+//const apiBaseUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const apiBaseUrl = "http://localhost:3000";
 import ProjektEditor from "./Projekteditor";
 
-/*
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.7);
-    left: 0;
-    top: 0;
-    z-index: 999999999999999999;
-*/
+const emptyProject = {
+  "name": "",
+  "description": "",
+  "url": "",
+  "img": "",
+  "date": "",
+  "state": ""
+}
 
 const Admin = () => {
-    const [projekty, setProjekty] = useState([])
-    const [openProjket, setOpenProjket] = useState()
-    useEffect(() => {
-        const pobierz = async () => {
-            try {
-                const projektyUrl = `${apiBaseUrl}/projects`;
-                const res = await fetch(projektyUrl);
+  const [projekty, setProjekty] = useState([])
+  const [openProjket, setOpenProjket] = useState()
 
-                if (!res.ok) {
-                    throw new Error("Nie udało się pobrać projektów.");
-                }
 
-                const projektys = await res.json();
-                setProjekty(projektys);
-            }
-            catch (error) {
-                console.error(error);
-            }
-        }
+  const pobierz = async () => {
+    try {
+      const projektyUrl = `${apiBaseUrl}/projects`;
+      const res = await fetch(projektyUrl);
 
-        pobierz();
-    }, []);
-    return (
-        
+      if (!res.ok) {
+        throw new Error("Nie udało się pobrać projektów.");
+      }
+
+      const projektys = await res.json();
+      setProjekty(projektys);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    pobierz();
+  }, []);
+
+  const update = (name, value) => setOpenProjket(o => ({ ...o, [name]: value }))
+  const close = () => setOpenProjket(null)
+
+
+
+
+  const save = async () => {
+    try {
+      const projektyUrl = `${apiBaseUrl}/project`;
+      const res = await fetch(projektyUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(openProjket),
+      });
+
+      if (!res.ok) {
+        throw new Error("Nie udało się pobrać projektów.");
+      }
+
+      pobierz()
+    }
+    catch (error) {
+      console.error(error);
+    }
+    close()
+  }
+
+
+  const deletee = async (id) => {
+    try {
+      const projektyUrl = `${apiBaseUrl}/project/${id}`;
+      const res = await fetch(projektyUrl, {
+        method: "DELETE",
+      });
+
+      if (!res.ok) {
+        throw new Error("Nie udało się pobrać projektów.");
+      }
+
+      pobierz()
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+
+
+
+
+  return (
     <section className="sekcja-projekty">
-        
-            
-        { openProjket && <ProjektEditor openProjket={openProjket} onClose={() => setOpenProjket(null)}/> }
+      <button onClick={() => setOpenProjket(emptyProject)}>otworz projekt</button>
+
+
+      {openProjket && <ProjektEditor openProjket={openProjket} update={update} save={save} onClose={close} />}
       <ul className="lista-projektow">
         {projekty.map((e) => {
 
           return (
-            <li key={`${e.name}-${e.date}`} className="karta-projektu">
+            <li key={`${e.name}-${e.id}`} className="karta-projektu">
               <button onClick={() => setOpenProjket(e)}>otworz projekt</button>
+              <button onClick={() => deletee(e.id)}>usuń</button>
               <h4 className="tytul-projektu">{e.name}</h4>
               <p className="opis-projektu">{e.description}</p>
               <a className="link-projektu" href={e.url} target="_blank" rel="noreferrer">
@@ -65,6 +121,6 @@ const Admin = () => {
         })}
       </ul>
     </section>
-    )
+  )
 }
 export default Admin
--- src/components/admin/ProjektEditor.jsx
+++ src/components/admin/ProjektEditor.jsx
@@ -1,17 +1,17 @@
 import "./ProjektEditor.css"
-const ProjektEditor = ({ openProjket, onClose }) => {
-    //<input value={} onChange={}/>-->
+const ProjektEditor = ({ openProjket, onClose, update, save }) => { 
 
     return (
         <section className="wrapper">
             <div className="popup">
-            <button onClick={onClose}>Zamknij</button>
-                <input value={openProjket.name} placeholder="wpisz nową nazwę" />
-                <input value={openProjket.url} placeholder="wpisz link"/>
-                <input value={openProjket.date} placeholder="wpisz datę utworzenia"/>
-                <input value={openProjket.state} placeholder="wpisz stan"/>
-                <input value={openProjket.img} placeholder="dodaj url obrazu"/>
-                <textarea placeholder="wpisz opis">{openProjket.description}</textarea>
+                <button onClick={onClose}>Zamknij</button>
+                <input value={openProjket.name} onChange={(e) => update('name', e.target.value)} placeholder="wpisz nową nazwę" />
+                <input value={openProjket.url} onChange={(e) => update('url', e.target.value)} placeholder="wpisz link" />
+                <input value={openProjket.date} onChange={(e) => update('date', e.target.value)} placeholder="wpisz datę utworzenia" />
+                <input value={openProjket.state} onChange={(e) => update('state', e.target.value)} placeholder="wpisz stan" />
+                <input value={openProjket.img} onChange={(e) => update('img', e.target.value)} placeholder="dodaj url obrazu" />
+                <textarea value={openProjket.description} onChange={(e) => update('description', e.target.value)} placeholder="wpisz opis"></textarea>
+                <button onClick={save}>save </button>
             </div>
         </section>
     )
